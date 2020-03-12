4dc948b257e9eb173257183bd1c0b6a7cec29b3d
chenlin <891635769@qq.com>
2020-03-12 18:05:10 +0800

    dsdssds

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -26,6 +26,7 @@
 			    success: function (res) {
 			        console.log('当前位置的经度：' + res.longitude);
 			        console.log('当前位置的纬度：' + res.latitude);
+					      console.log('当前位置的纬度23：' + res.latitude);
 			    }
 			});
 			
